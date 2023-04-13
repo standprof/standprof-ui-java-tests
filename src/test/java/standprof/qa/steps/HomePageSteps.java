c2b88f2a728cb198ff7449a37a56ffb6b41b4393
Stan Desyatnikov <info@standprof.co.uk>
2023-04-13 08:01:45 +0100

    Simplify asserts. Remove drivers hashtable

--- src/test/java/standprof/qa/steps/HomePageSteps.java
+++ src/test/java/standprof/qa/steps/HomePageSteps.java
@@ -6,6 +6,8 @@ import org.junit.Assert;
 import standprof.qa.base.UiBase;
 import standprof.qa.pages.HomePage;
 
+import static org.junit.Assert.*;
+
 public class HomePageSteps extends UiBase {
 
     private final HomePage homePage;
@@ -16,13 +18,11 @@ public class HomePageSteps extends UiBase {
 
     @Given("^I am on the Home page$")
     public void iAmOnTheHomePage() {
-        System.out.format("Thread ID - %2d.\n",
-                Thread.currentThread().getId());
         homePage.goToHomePage();
     }
 
     @Then("^I should see the menu$")
     public void iShouldSeeMenu() {
-        Assert.assertTrue(homePage.isMenuDisplayed());
+        assertTrue(homePage.isMenuDisplayed());
     }
 }
--- src/test/java/standprof/qa/steps/hooks/Hooks.java
+++ src/test/java/standprof/qa/steps/hooks/Hooks.java
@@ -2,16 +2,12 @@ package standprof.qa.steps.hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import standprof.qa.base.UiBase;
 
-import java.util.Hashtable;
-
 public class Hooks extends UiBase {
 
-    private UiBase uiBase;
-    private  Hashtable drivers = new java.util.Hashtable<Integer, WebDriver>();
+    private final UiBase uiBase;
 
     public Hooks(UiBase uiBase){
         this.uiBase = uiBase;
@@ -22,13 +18,11 @@ public class Hooks extends UiBase {
     {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         uiBase.driver = new ChromeDriver();
-        drivers.put(uiBase.driver.hashCode(), uiBase.driver);
     }
 
     @After()
     public void stopDriver()
     {
-        WebDriver driver = (WebDriver) drivers.get(uiBase.driver.hashCode());
-        driver.quit();
+        uiBase.driver.quit();
     }
 }
